5730f6ba599a6c78ed7007dc6bb9bb9373c58379
frostnight <frostnightsong@gmail.com>
2024-09-14 17:45:40 +0900

    예산 문제 완료

--- src/main/java/frostnight/programmers/level1/Budget.java
+++ src/main/java/frostnight/programmers/level1/Budget.java
@@ -1,12 +1,17 @@
 package frostnight.programmers.level1;
 
+import java.util.Arrays;
+
 public class Budget {
 
   public static void main(String[] args) {
-    int[][] d = {};
-    int[] budget = {};
-    int[] excepted = {};
+    int[][] d = {
+      {1, 3, 2, 5, 4}, {2, 2, 3, 3}, {9, 4, 3, 10}, {7, 3, 10, 12, 5, 7, 8, 15, 6, 1, 4}
+    };
+    int[] budget = {9, 10, 18, 25};
+    int[] excepted = {3, 4, 3, 5};
     for (int i = 0; i < d.length; i++) {
+      System.out.println("department>> " + solution(d[i], budget[i]));
       if (excepted[i] != solution(d[i], budget[i])) {
         System.out.println("index error = " + i);
         break;
@@ -15,7 +20,23 @@ public class Budget {
   }
 
   static int solution(int[] d, int budget) {
-    System.out.println("solution");
-    return 0;
+    int admissionCount = 0;
+    // 1. 작은 순으로 정렬 한다.
+    Arrays.sort(d);
+    // 2. 예산를 깎으면서 사용한 예산을 합한다.
+    int index = 0;
+    while (budget > 0 && index < d.length) {
+      int departmentBudget = d[index];
+
+      // 3. 예산이 다 떨어지면 멈춘다.
+      if (budget - departmentBudget < 0) {
+        break;
+      }
+
+      admissionCount++;
+      budget -= departmentBudget;
+      index++;
+    }
+    return admissionCount;
   }
 }
