c2bc0155da83e87cb27f0a892560fd5ac7ff3165
frostnight <frostnightsong@gmail.com>
2025-02-08 19:59:47 +0900

    10 같은 2자리 이상 숫자 해결 중

--- src/main/java/frostnight/programmers/level1/KakaoDartGame.java
+++ src/main/java/frostnight/programmers/level1/KakaoDartGame.java
@@ -21,6 +21,8 @@ public class KakaoDartGame {
     // 1. 정수로 변환해서 숫자면 점수 로직 아니면 보너스 로직
     // 정수면 합산에 일단 포함, 보너스면 로직에 따라 이전 점수를 변환
     String[] dartSplit = dartResult.split("");
+    char[] charArray = dartResult.toCharArray();
+
     List<Integer> points = new ArrayList<>();
     for (int i = 0; i < dartSplit.length; i++) {
       try {
@@ -53,12 +55,18 @@ public class KakaoDartGame {
 
   public static void main(String[] args) {
     String[] dartResult = {
-      "1S2D*3T", /*"1D2S#10S", "1D2S0T", "1S*2T*3S", "1D#2S*3S", "1T2D3D#", "1D2S3T*"*/
+      //      "1S2D*3T", "1D2S#10S", "1D2S0T", "1S*2T*3S", "1D#2S*3S", "1T2D3D#", "1D2S3T*"
+      "1D2S#10S"
     };
-    int[] answer = {37, 9, 3, 23, 5, -4, 59};
+    //    int[] answer = {37, 9, 3, 23, 5, -4, 59};
+    int[] answer = {9};
     for (int i = 0; i < dartResult.length; i++) {
       int excepted = solution(dartResult[i]);
-      assert excepted == answer[i];
+      System.out.println("(excepted == answer[i]) = " + (excepted == answer[i]));
+      if (excepted != answer[i]) {
+        System.out.println("dartResult[i] = " + dartResult[i]);
+        System.out.println("excepted = " + excepted);
+      }
     }
   }
 }
