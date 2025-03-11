1d9a356203cfa5121ed3e1fdfd0e97eacd4a23c2
frostnight <frostnightsong@gmail.com>
2025-03-12 00:03:39 +0900

    두개 골라서 오름차순 합 완료

--- src/main/java/frostnight/programmers/level1/TwoSelectSum.java
+++ src/main/java/frostnight/programmers/level1/TwoSelectSum.java
@@ -1,9 +1,6 @@
 package frostnight.programmers.level1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return
@@ -11,28 +8,23 @@ import java.util.Map;
  */
 public class TwoSelectSum {
   public int[] solution(int[] numbers) {
-    int[] answer = {};
-    List<Integer> result = new ArrayList<>();
-    Map<String, Integer> memo = new HashMap<>();
+    Set<Integer> result = new HashSet<>();
     for (int i = 0; i < numbers.length; i++) {
-      for (int j = i; j < numbers.length; j++) {
-        if (i != j) {
-          memo.put(String.valueOf(i) + "," + String.valueOf(j), numbers[i] + numbers[j]);
-        }
+      for (int j = i + 1; j < numbers.length; j++) {
+        result.add(numbers[i] + numbers[j]);
       }
     }
-    System.out.println("result = " + memo);
-    return answer;
+    return result.stream().sorted().mapToInt(Integer::intValue).toArray();
   }
 
   public static void main(String[] args) {
     TwoSelectSum twoSelectSum = new TwoSelectSum();
-    int[][] numbers = {{2, 1, 3, 4, 1}, {5, 0, 2, 7}};
-    int[][] except = {{2, 3, 4, 5, 6, 7}, {2, 5, 7, 9, 12}};
+    int[][] numbers = {{2, 1, 3, 4, 1}, {5, 0, 2, 7}, {0,3,3,4,4,23,12}};
+    int[][] except = {{2, 3, 4, 5, 6, 7}, {2, 5, 7, 9, 12}, {3, 4, 6, 7, 8, 12, 15, 16, 23, 26, 27, 35}};
 
     for (int i = 0; i < numbers.length; i++) {
       int[] result = twoSelectSum.solution(numbers[i]);
-      for (int j = 0; j < except.length; j++) {
+      for (int j = 0; j < numbers[i].length; j++) {
         if (result[j] != except[i][j]) {
           System.out.println("i = " + i);
           System.out.println("except[i][j] = " + except[i][j]);
