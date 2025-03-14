2d8b1cc393570924ddaa98bc3a9c49984e8f7370
frostnight <frostnightsong@gmail.com>
2025-03-14 23:55:41 +0900

    3진법 뒤집기 완료

--- src/main/java/frostnight/programmers/level1/TernarySystemReverse.java
+++ src/main/java/frostnight/programmers/level1/TernarySystemReverse.java
@@ -3,8 +3,9 @@ package frostnight.programmers.level1;
 public class TernarySystemReverse {
 
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        String ternary = Integer.toString(n, 3);
+        String reverseTernary = new StringBuffer(ternary).reverse().toString();
+        return Integer.parseInt(reverseTernary, 3);
     }
 
     public static void main(String[] args) {
