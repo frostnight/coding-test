d02ac445c6526cd37513eadc6f87d2cf10647d51
frostnight <frostnightsong@gmail.com>
2025-04-12 09:03:44 +0900

    midSave

--- src/main/java/frostnight/programmers/level1/MinimumRectangle.java
+++ src/main/java/frostnight/programmers/level1/MinimumRectangle.java
@@ -1,48 +1,45 @@
 package frostnight.programmers.level1;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinimumRectangle {
+
+    // 1. 가로, 세로가 모든 지갑 크기보다는 커야 한다.
 
     public int solution(int[][] sizes) {
         int answer = 0;
         int maxWidth = 0, maxHeight = 0;
-        TreeSet<Integer> widthSet = new TreeSet<>(Comparator.reverseOrder());
-        TreeSet<Integer> heightSet = new TreeSet<>(Comparator.reverseOrder());
-        /**
-         * {
-         * index
-         * number
-         * }
-         */
-
-        // 1. 가로, 세로 각 각 리스트로 변환
+        List<int[]> newSizes = new ArrayList<int[]>();
+        // 우선 제일 큰 지갑 사이즈를 구한다.
         for (int[] size : sizes) {
-            widthSet.add(size[0]);
-            heightSet.add(size[1]);
+            int width = size[0];
+            int height = size[1];
+            newSizes.add(size);
+            newSizes.add(new int[] {height, width});
         }
-        // 역순 정렬 및 첫 번째 값 max으로 SET
-        maxWidth = widthSet.first();
-        maxHeight = heightSet.first();
-        for (int[] size : sizes) {
-            if ((size[0] >= maxWidth || size[1] >= maxHeight) &&
-                    (size[1] <= maxWidth && size[0] <= maxHeight)) {
-                System.out.println("maxWidth = " + maxWidth);
-                System.out.println("maxHeight = " + maxHeight);
-                System.out.println("size = " + size[0] + "," + size[1]);
-                // 서로 위치 변환 후 둘 다 최대치 보다 낮은 경우 서로 값을 바꿔서 계산
-                if (size[0] >= maxWidth) {
-                    widthSet.pollFirst();
-                    maxWidth = Optional.ofNullable(widthSet.first()).orElse(0);
-                }
-                if (size[1] >= maxHeight) {
-                    heightSet.pollFirst();
-                    maxHeight = Optional.ofNullable(heightSet.first()).orElse(0);
+
+        for (int i = 0; i < newSizes.size(); i++) {
+            int width = newSizes.get(i)[0];
+            int height = newSizes.get(i)[1];
+            for (int j = 0; j < newSizes.size(); j++) {
+                if (i != j) {
+                    int compWidth = newSizes.get(j)[0];
+                    int compHeight = newSizes.get(j)[1];
                 }
             }
         }
-        answer = maxHeight * maxWidth;
+//        for (int i = 0; i < newSizes.size(); i++) {
+//            int width = newSizes.get(i)[0];
+//            int height = newSizes.get(i)[1];
+//            if ((width >= maxWidth || height >= maxHeight) && width * height < answer) {
+//                maxWidth = width;
+//                maxHeight = height;
+//                answer = width * height;
+//            }
+//        }
+//        System.out.println("answer2 = " + answer);
+
         return answer;
     }
 
@@ -50,8 +47,8 @@ public class MinimumRectangle {
 
         MinimumRectangle minimumRectangle = new MinimumRectangle();
         int[][][] numbers = {
-//                {{60, 50}, {30, 70}, {60, 30}, {80, 40}},
-                {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}},
+                {{60, 50}, {30, 70}, {60, 30}, {80, 40}},
+//                {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}},
 //                {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}},
         };
         int[] except = {4000, 120, 133};
