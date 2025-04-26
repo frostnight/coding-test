7c86abb0c49da64af3fe4bc15d7fdc539174297d
frostnight <frostnightsong@gmail.com>
2025-04-26 16:44:50 +0900

    숫자 짝꿍 완료

--- src/main/java/frostnight/programmers/level1/NumberMate.java
+++ src/main/java/frostnight/programmers/level1/NumberMate.java
@@ -1,49 +1,55 @@
 package frostnight.programmers.level1;
 
 import java.util.*;
-
-/**
- * 두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다
- * (단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다). X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
- *
- * 예를 들어, X = 3403이고 Y = 13203이라면, X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다.
- * 다른 예시로 X = 5525이고 Y = 1255이면 X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다
- * (X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
- * 두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
- *
- * 제한사항
- * 3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
- * X, Y는 0으로 시작하지 않습니다.
- * X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
- */
+import java.util.stream.Collectors;
 
 public class NumberMate {
 
-    public String solution(String X, String Y) {
-        String answer = "";
-        List<String> Xlist = new ArrayList<>(Arrays.asList(X.split("")));
-        List<String> Ylist = new ArrayList<>(Arrays.asList(Y.split("")));
-        Set<String> resultSet = new HashSet<>();
-        // 두 집단을 비교해서 X와 Y중 겹치는 문자를 하나의 셋에 넣고 비교군에서 제외시킨다.
-        for (String x : Xlist) {
-            for (String y : Ylist) {
-                if (x.equals(y)) {
-                    resultSet.add(x);
-                    Ylist.remove(y);
-                }
-            }
+  public String solution(String X, String Y) {
+    StringBuilder answer = new StringBuilder();
+    List<String> Xlist = new ArrayList<>(Arrays.asList(X.split("")));
+    Map<String, Long> YMap =
+        Arrays.stream(Y.split("")).collect(Collectors.groupingBy(y -> y, Collectors.counting()));
+    List<String> resultList = new ArrayList<>();
+    // 두 집단을 비교해서 X와 Y중 겹치는 문자를 하나의 셋에 넣고 비교군에서 제외시킨다.
+    for (String x : Xlist) {
+      for (Map.Entry<String, Long> yEntry : YMap.entrySet()) {
+        if (x.equals(yEntry.getKey()) && yEntry.getValue() > 0L) {
+          resultList.add(x);
+          yEntry.setValue(yEntry.getValue() - 1L);
+          break;
         }
-        // 만약 아무것도 없으면 -1, 0만 있으면 0을 문자열로 리턴
-        // Set 내부를 역순으로 정렬하고 하나의 String으로 반환
-        return answer;
+      }
     }
-
-    public static void main(String[] args) {
-        NumberMate numberMate = new NumberMate();
-        String[] X = {"100"};
-        String[] Y = {"2345"};
-        String[] RESULT = {"-1"};
-
-        numberMate.solution(X[0], Y[0]);
+    // 만약 아무것도 없으면 -1, 0만 있으면 0을 문자열로 리턴
+    if (resultList.isEmpty()) {
+      return "-1";
+    } else if (resultList.stream().allMatch(r -> r.equals("0"))) {
+      return "0";
     }
+    // 내림차순 정렬
+    resultList.sort(Collections.reverseOrder());
+    // resultList를 String으로 변환
+    for (String s : resultList) {
+      answer.append(s);
+    }
+    // String으로 반환
+    return answer.toString();
+  }
+
+  public static void main(String[] args) {
+    NumberMate numberMate = new NumberMate();
+    String[] X = {"100", "100", "100", "12321", "5525", "4362318"};
+    String[] Y = {"2345", "203045", "123450", "42531", "1255", "4338514"};
+    String[] RESULT = {"-1", "0", "10", "321", "552", "84331"};
+
+    for (int i = 0; i < X.length; i++) {
+      String result = numberMate.solution(X[i], Y[i]);
+      if (!RESULT[i].equals(result)) {
+        System.out.println("result = " + result);
+        System.out.println("RESULT[i] = " + RESULT[i]);
+        System.out.println("i = " + i);
+      }
+    }
+  }
 }
