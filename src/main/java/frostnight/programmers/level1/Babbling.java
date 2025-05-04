a0e65ad09ac0203c3904f956833d675b27108a97
frostnight <frostnightsong@gmail.com>
2025-05-04 20:24:51 +0900

    옹알이 완료

--- src/main/java/frostnight/programmers/level1/Babbling.java
+++ src/main/java/frostnight/programmers/level1/Babbling.java
@@ -1,10 +1,31 @@
 package frostnight.programmers.level1;
 
-public class Babbling {
+import java.util.List;
 
-    public int solution(String[] babbling) {
+public class Babbling {
+    static final List<String> canPronunciations = List.of("aya", "ye", "woo", "ma");
+    public int solution(String[] babblings) {
         int answer = 0;
+        for (String babbling : babblings) {
+            if (findPronunciation(babbling, "")) {
+                answer++;
+            }
+        }
         return answer;
+    }
+
+    public boolean findPronunciation(String babbling, String prevPronunciation) {
+
+        if ("".equals(babbling)) return true;
+
+        for (int i = 0; i < canPronunciations.size(); i++) {
+            String canPronunciation = canPronunciations.get(i);
+            if (babbling.startsWith(canPronunciation) && !prevPronunciation.equals(canPronunciation)) {
+                String substring = babbling.substring(canPronunciation.length());
+                return findPronunciation(substring, canPronunciation);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
