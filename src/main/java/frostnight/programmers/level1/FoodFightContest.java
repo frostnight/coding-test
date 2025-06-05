395320469d708103d4045f08f2a6ccaf1c6fc551
frostnight <frostnightsong@gmail.com>
2025-06-06 03:06:02 +0900

    init

---
+++ src/main/java/frostnight/programmers/level1/FoodFightContest.java
@@ -0,0 +1,13 @@
+package frostnight.programmers.level1;
+
+public class FoodFightContest {
+
+    public String solution(int[] food) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
